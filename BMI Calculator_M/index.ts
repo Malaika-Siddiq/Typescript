b40052ec36edb432a60ee2d0ce434585281f51e3
Malaika-Siddiq <siddiqmoiz196@gmail.com>
2024-03-27 23:35:01 +0500

    complete

---
+++ BMI Calculator_M/index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter firstNumber", type: "number", name: "firstNumber" },
+  { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition","Subtraction","Multiplication","Division"],
+  },
+]);  
+
+
+// Conditional Statement
+if (answer.operator === "Addition"){
+  console.log(answer.firstNumber + answer.secondNumber);
+  
+} else if(answer.operator === "Subtraction"){
+  console.log(answer.firstNumber - answer.secondNumber);
+  
+} else if(answer.operator === "Multiplication"){
+  console.log(answer.firstNumber * answer.secondNumber);
+  
+} else if (answer.operator === "Division"){
+  console.log(answer.firstNumber / answer.secondNumber);
+  
+} else {
+  console.log("please select valid operator");
+  
+}
